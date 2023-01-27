afc051d617fd46b3f24c951db809e69cdf035464
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2023-01-27 21:27:54 +0100

    setup inputs for pers info and work experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TextInput } from './textInput';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'better-cv-maker';
+  title = 'Better CV Maker';
+  personalInfoInputs: Array<TextInput> = [
+    { label: "Name", type: "text", value: "" },
+    { label: "Adresse", type: "text", value: "" },
+    { label: "Phone", type: "text", value: "" },
+    { label: "LinkedIn", type: "text", value: "" },
+    { label: "website", type: "text", value: "" },
+  ];
+  workExperienceInputs: Array<WorkExperienceInput> = [
+    { label: "Name", type: "text", value: "" },
+    { label: "Adresse", type: "text", value: "" },
+    { label: "Phone", type: "text", value: "" },
+    { label: "LinkedIn", type: "text", value: "" },
+    { label: "website", type: "text", value: "" },
+  ];
+
+  newTextInput: TextInput = {
+    label: '',
+    type: 'text',
+    value: ''
+  }
+
+  addInput(): void {
+    this.inputs = [...this.inputs, this.newTextInput];
+  }
 }
---
+++ src/app/workExperienceInput.ts
@@ -0,0 +1,7 @@
+export interface WorkExperienceInput {
+  label: string,
+  title: string,
+  description: string,
+  time: string,
+  appliedSkills: Array<String>
+}
